23f9bc876bddeffa0290b6a343d8298f8b30e94a
Cekiso <nonkululeko@quirky30.co.za>
2021-06-08 22:35:42 +0200

    new work

--- reg.Dom.js
+++ reg.Dom.js
@@ -8,17 +8,22 @@ var regMsg = document.querySelector(".message");
 
 
 //style
-document.querySelector(".message").style.border = "1px solid blue ";
-document.querySelector(".message").style.fontFamily = "Arial";
-//document.querySelector(".message").style.fontSize = "larger";
-document.querySelector(".message").style.display = "inline";
+// document.querySelector(".message").style.border = "1px solid blue ";
+// document.querySelector(".message").style.fontFamily = "Arial";
+// document.querySelector(".message").style.fontSize = "larger";
+// document.querySelector(".message").style.display = "inline";
 //style
 
 //for loop for the RegNo
-var div = document.getElementsByClassName("regNo");
-for (var i = 0; i < div.length; i++) {
 
-}
+// var divs = document.getElementsByClassName("message");
+// for (var i=0; i < divs.length; i++)
+// {   var test = document.createElement("divs1") 
+//     test.innerHTML = 'test'
+//     divs[i].style.border = "1px solid blue"
+// }
+// var test = document.createElement("divs1") 
+// test.innerHTML = 'test'
 
 //declaring the variable to store existing names from the localstorage.
 var existingNames;
@@ -30,25 +35,29 @@ if (localStorage['regNo']) {
 }
 
 var Reginstance = Reg(existingNames)
+regMsg.innerHTML = Reginstance.Names(RegNo.value).join(" | ");
 
 function regNumber() {
     //var addButton = document.querySelector("input[name='place']:checked");
     var addButton = document.createElement("BUTTON");
+    
     if (addButton) {
         Reginstance.setReg(RegNo.value);
-        regMsg.innerHTML = Reginstance.Names(RegNo.value)
+        regMsg.innerHTML = Reginstance.Names(RegNo.value).join(" | ");
         localStorage['regNo'] = JSON.stringify(Reginstance.Names(RegNo.value, Regbutton.value));
 
         // document.body.appendChild(addButton);
         //console.log(number)
     }
+    RegNo.value = "";
 }
+
 
 //gonna pull/ show the RegNo that are added
 function pullRegNo() {
     var addRadio = document.querySelector('input[type=radio][name=place]:checked').value;
     if (addRadio) {
-        regMsg.innerHTML = Reginstance.number(addRadio)
+        regMsg.innerHTML = Reginstance.number(addRadio).join(" | ");
     }
 
 }
